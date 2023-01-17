ffdbb54c3bcefcf38fa41c3d5d3d4e1fab1ab740
Kbikash24 <bikashk11@gmail.com>
2023-01-17 13:04:59 +0530

    new changes

--- newjs.js
+++ newjs.js
@@ -13,10 +13,16 @@ var headerTitle=document.getElementById('header-title')
 //headerTitle.innerHTML='<h3>hello</h>'
 headerTitle.style.borderBottom='solid 3px black'
 //header.style.borderBottom='solid 3px black'
-//var items=document.getElementsByClassName('list-group-item');
+var items=document.getElementsByClassName('list-group-item');
+console.log(items)
+items[2].style.background='green'
+items[0].style.fontWeight='700'
+items[2].style.fontWeight='700'
+items[3].style.fontWeight='700'
+items[1].style.fontWeight='700'
 //var title=document.getElementsByClassName('.title');
 
-var titles=document.querySelectorAll('.title')
-console.log(titles)
-titles[0].style.color='green'
-titles[0].style.fontweight='bold'
+//var titles=document.querySelectorAll('.title')
+//console.log(titles)
+//titles[0].style.color='green'
+
