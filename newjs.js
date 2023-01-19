822e698b943f6e6e7695c0150e2dcf142887a3bf
Kbikash24 <bikashk11@gmail.com>
2023-01-19 14:21:11 +0530

    adding elements in DOM

--- newjs.js
+++ newjs.js
@@ -1,17 +1,17 @@
-console.log(document.domain)
-console.log(document.URL)
+//console.log(document.domain)
+///console.log(document.URL)
 
-console.log(document.title)
+//console.log(document.title)
 //console.log(document.doctype)
 //console.log(document.head)
 //console.log(document.all)
 //document.all[10].textContent='hell0'
 //console.log(document.getElementById('header-title'));
-var headerTitle=document.getElementById('header-title')
+//var headerTitle=document.getElementById('header-title')
 //var header=document.getElementById('main-header')
 //console.log(headerTitle)
 //headerTitle.innerHTML='<h3>hello</h>'
-headerTitle.style.borderBottom='solid 3px black'
+//headerTitle.style.borderBottom='solid 3px black'
 //header.style.borderBottom='solid 3px black'
 //var items=document.getElementsByClassName('list-group-item');
 //console.log(items)
@@ -50,7 +50,7 @@ input.value='HELLO WORLD'
  thirdItem.style.background='white'
  thirdItem.style.display='none'*/
   // QUERYSELECTORALL
-   var titles=document.querySelectorAll('.title')
+   /*var titles=document.querySelectorAll('.title')
    console.log(titles)
    titles[0].textContent='Hello'
    var odd= document.querySelectorAll('li:nth-child(odd)');
@@ -58,4 +58,66 @@ input.value='HELLO WORLD'
    for(var i=0;i<odd.length;i++){
    odd[i].style.backgroundColor='green'
    even[i].style.backgroundColor='#ccc';
-   }
+   }*/
+   //TRAVERSE THE DOM
+   
+//var itemList=document.querySelector('#items')
+//parentNode
+//console.log(itemList.parentNode)
+//itemList.parentNode.style.backgroundColor='#f4f4f4'
+//console.log(itemList.parentNode.parentNode.parentNode)
+
+//parentElement
+//console.log(itemList.parentElement)
+//itemList.parentElement.style.backgroundColor='#f4f4f4'
+//console.log(itemList.parentElement.parentElement.parentElement)
+
+//childNodes
+//console.log(itemList.childNodes)
+//console.log(itemList.children)
+//console.log(itemList.children[1])
+//itemList.children[1].style.backgroundColor='yellow'
+ 
+//Firstchild
+
+//console.log(itemList.firstChild)
+//firstElementChild
+//console.log(itemList.firstElementChild)
+//itemList.firstElementChild.textContent='hello 1'
+
+//lastchild 
+
+//console.log(itemList.lastChild)
+//lastElementChild
+//console.log(itemList.firstElementChild)
+//itemList.firstElementChild.textContent='hello 4'
+
+//nextsibling
+//console.log(itemList.nextSibling);
+//nextElementSibling
+//console.log(itemList.nextElementSibling)
+
+//previousSibling
+//console.log(itemList.previousSibling)
+//previousSibling
+//console.log(itemList.previousElementSibling)
+//itemList.previousElementSibling.style.color='green'
+  //const para=document.createElement("class")
+  
+// Get the title element
+var titles = document.querySelector('.container');
+
+// Create a new text node with the text "Hello"
+var text = document.createTextNode("Hello ");
+
+// Insert the text node before the title element
+titles.insertBefore(text,titles.firstChild);
+
+//Get the title element
+var item = document.querySelector('.list-group');
+
+// Create a new text node with the text "Hello"
+var text = document.createTextNode("Hello ");
+
+// Insert the text node before the title element
+item.insertBefore(text,item.firstChild);
